5d1cbdba50f72ff953f2d2acd59259a1ccc7aef9
Mario Cao <mariocao@gmail.com>
2023-10-02 16:27:45 +0000

    feat(cli): add wasm show command

--- libs/cli/src/commands/wasm/index.ts
+++ libs/cli/src/commands/wasm/index.ts
@@ -1,8 +1,10 @@
 import { Command } from 'commander';
 import { upload } from './upload.js';
 import { list } from './list.js';
+import { show } from './show.js';
 
 export const wasm = new Command('wasm');
 wasm.description('commands related to WASM binaries');
 wasm.addCommand(upload);
 wasm.addCommand(list);
+wasm.addCommand(show);
--- libs/cli/src/services/wasm/query.ts
+++ libs/cli/src/services/wasm/query.ts
@@ -1,6 +1,7 @@
 import { QueryClient, createProtobufRpcClient } from '@cosmjs/stargate';
 import { QueryClientImpl } from '../../gen/sedachain/wasm_storage/v1/query.js';
 import { Tendermint34Client } from '@cosmjs/tendermint-rpc';
+import { WasmType } from '../../gen/sedachain/wasm_storage/v1/wasm_storage.js';
 
 export async function queryDataRequestWasms(endpoint: string) {
   const queryService = await buildQueryService(endpoint);
@@ -18,6 +19,26 @@ export async function queryDataRequestWasms(endpoint: string) {
   return result;
 }
 
+export async function queryDataRequestWasm(endpoint: string, hash: string) {
+  const queryService = await buildQueryService(endpoint);
+  const queryResult = await queryService.DataRequestWasm({
+    hash,
+  });
+
+  // If wasm does not exist, it returns a result with empty/zeroed fields
+  if (!queryResult.wasm || queryResult.wasm.hash.length == 0) {
+    throw Error('Data Request WASM binary not found');
+  }
+
+  // Transform to something that can be displayed
+  return {
+    hash: toHexString(queryResult.wasm?.hash),
+    type: WasmType[queryResult.wasm?.wasmType],
+    timestamp: queryResult.wasm?.addedAt?.toUTCString(),
+    bytes: queryResult.wasm?.bytecode.length,
+  };
+}
+
 async function buildQueryService(endpoint: string) {
   // Tendermint RPC endpoint
   const tendermintClient = await Tendermint34Client.connect(endpoint);
@@ -32,4 +53,8 @@ async function buildQueryService(endpoint: string) {
   return new QueryClientImpl(rpcClient);
 }
 
+function toHexString(byteArray: Uint8Array) {
+  return Array.from(byteArray, function (byte) {
+    return ('0' + (byte & 0xff).toString(16)).slice(-2);
+  }).join('');
 }
