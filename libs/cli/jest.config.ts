da1948915865673f79e1317b5a9a521bc06a552b
Thomas van Dam <thomas@seda.xyz>
2024-06-20 17:37:37 +0200

    chore(cli): remove old CLI code
    Everything was migrated to the new dev-tools package.

--- libs/cli/jest.config.ts
+++
@@ -1,6 +0,0 @@
-/* eslint-disable */
-export default {
-  displayName: 'cli',
-  preset: '../../jest.preset.mjs',
-  coverageDirectory: '../../coverage/libs/cli',
-};
--- libs/cli/src/index.ts
+++
@@ -1,3 +0,0 @@
-import { commandInit } from './commands/index.js';
-
-commandInit();
