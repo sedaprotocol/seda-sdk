ced0dddaabb41f144655519ead40239a4f974c3d
Thomas van Dam <thomas@seda.xyz>
2024-06-24 11:50:06 +0200

    fix(sdk-tools): include default cosmwasm types in codec registry

--- libs/dev-tools/src/lib/services/signing-client.ts
+++ libs/dev-tools/src/lib/services/signing-client.ts
@@ -1,6 +1,4 @@
 import { SigningCosmWasmClient } from '@cosmjs/cosmwasm-stargate';
-import { Registry } from '@cosmjs/proto-signing';
-import { defaultRegistryTypes } from '@cosmjs/stargate';
 import { Result } from 'true-myth';
 import { sedachain } from '@dev-tools/proto';
 import { tryAsync } from '@dev-tools/utils/try-async';
@@ -11,22 +9,20 @@ export async function createSigningClient(
 ): Promise<
   Result<{ client: SigningCosmWasmClient; address: string }, unknown>
 > {
-  const registry = new Registry(defaultRegistryTypes);
-  registry.register(
-    '/sedachain.wasm_storage.v1.MsgStoreDataRequestWasm',
-    sedachain.wasm_storage.v1.MsgStoreDataRequestWasm
-  );
-
   const signingClientResult = await tryAsync(async () =>
     SigningCosmWasmClient.connectWithSigner(
       signer.getEndpoint(),
-      signer.getSigner(),
-      { registry }
+      signer.getSigner()
     )
   );
   if (signingClientResult.isErr) {
     return Result.err(signingClientResult.error);
   }
+
+  signingClientResult.value.registry.register(
+    '/sedachain.wasm_storage.v1.MsgStoreDataRequestWasm',
+    sedachain.wasm_storage.v1.MsgStoreDataRequestWasm
+  );
 
   return Result.ok({
     client: signingClientResult.value,
