b6420bc3341bf27a54e2c788e84188c34e3ecfef
Thomas van Dam <thomas@seda.xyz>
2024-06-24 10:03:20 +0200

    chore(as-sdk): export JSON library
    This allows consumers to reuse the library so they don't have to import
    it or another JSON library themselves.
    
    Part-of: #35

--- libs/as-sdk/assembly/index.ts
+++ libs/as-sdk/assembly/index.ts
@@ -8,6 +8,8 @@ export {
   HttpResponse,
 } from './http';
 
+// Export library so consumers don't need to reimport it themselves
+export { JSON } from 'json-as/assembly';
 export { PromiseStatus } from './promise';
 export { Process, Tally };
 export { RevealBody } from './tally';
