d22e9937e008f2ea6006b8ead15e830a132df85b
Mario Cao <mariocao@gmail.com>
2025-05-22 16:23:53 +0000

    chore: expose encode consensus filter

--- libs/dev-tools/src/index.ts
+++ libs/dev-tools/src/index.ts
@@ -4,16 +4,17 @@ export { Signer, type ISigner } from "@dev-tools/services/signer";
 export { uploadOracleProgram } from "@dev-tools/services/oracle-program/upload-oracle-program";
 export { getOracleProgram } from "@dev-tools/services/oracle-program/get-oracle-program";
 
-export { postDataRequest } from "@dev-tools/services/dr/post-data-request";
-export { postDataRequestBundle } from "@dev-tools/services/dr/post-data-request-bundle";
-export { getDataRequestStatus } from "@dev-tools/services/dr/get-data-request-status";
-export { getDataRequestBundleStatus } from "@dev-tools/services/dr/get-data-request-bundle-status";
 export { awaitDataResult } from "@dev-tools/services/dr/await-data-result";
 export { awaitDataResultBundle } from "@dev-tools/services/dr/await-data-result-bundle";
+export { encodeConsensusFilter } from "@dev-tools/services/dr/consensus-filter";
+export { getDataRequestBundleStatus } from "@dev-tools/services/dr/get-data-request-bundle-status";
+export { getDataRequestStatus } from "@dev-tools/services/dr/get-data-request-status";
 export { getDataResult } from "@dev-tools/services/dr/get-data-result";
 export { getDataResultBundle } from "@dev-tools/services/dr/get-data-result-bundle";
 export { postAndAwaitDataRequest } from "@dev-tools/services/dr/post-and-await-data-request";
 export { postAndAwaitDataRequestBundle } from "@dev-tools/services/dr/post-and-await-data-request-bundle";
+export { postDataRequest } from "@dev-tools/services/dr/post-data-request";
+export { postDataRequestBundle } from "@dev-tools/services/dr/post-data-request-bundle";
 
 export * from "./lib/testing/create-mock-reveal";
 export * from "./lib/testing/create-mock-tally-args";
